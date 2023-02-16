b78294f45649c74a6ffea6018cf913dad89f3811
Matthew Mayfield <holophysics@gmail.com>
2023-02-15 19:56:55 -0600

    rendered a wire

--- main.rb
+++ main.rb
@@ -4,12 +4,12 @@
 
 class Circuit
   def initialize graph
-    self.graph = graph
+    @graph = graph
   end
 end
 
 class Solid
-  attr_accessor :x, :y, :w, :h, :r, :g, :b, :a_x
+  attr_accessor :x, :y, :w, :h, :r, :g, :b, :a, :blendmode_enum
 
   def primitive_marker
     :solid
@@ -17,34 +17,68 @@ class Solid
 end
 
 class Wire < Solid
-  width = 6
-  def initialize x1, y1, x2, y2
-    self.x = get_x x1, y1, x2, y2
-    self.y = y
-    self.w = w
-    self.h = h
+
+  def initialize x1, y1, x2, y2, w
+    self.x = get_x x1, y1, x2, y2, w
+    self.y = get_y x1, y1, x2, y2, w
+    self.w = get_w x1, y1, x2, y2, w
+    self.h = get_h x1, y1, x2, y2, w
+  end
+
+  def serialize
+    {
+      x: x,
+      y: y,
+      w: w,
+      h: h
+    }
+  end
+
+  def inspect
+    serialize.to_s
+  end
+
+  def to_s
+    serialize.to_s
   end
   #===========================================================================
-  def get_x x1, y1, x2, y2
+  def get_x x1, y1, x2, y2, w
     if x2 == x1
-      x = x1 - (width / 2).round
+      x = x1 - (w / 2).round
     elsif y1 == y2
       x = x1
     end
     x
   end
   #===========================================================================
-  def get_y x1, y1, x2, y2
+  def get_y x1, y1, x2, y2, w
     if x2 == x1
       y = y1
     elsif y2 == y1
-      y = y1 - (width / 2).round
+      y = y1 - (w / 2).round
     end
-    
-    
+    y
   end
-
+  #===========================================================================
+  def get_w x1, y1, x2, y2, w
+    if x2 == x1
+      w = w
+    elsif y2 == y1
+      w = x2 - x1
+    end
+    w
+  end
+  #===========================================================================
+  def get_h x1, y1, x2, y2, w
+    if x2 == x1
+      h = y2 - y1
+    elsif y2 == y1
+      h = w
+    end
+    h
+  end
 end
+
 
 class Sprite
   attr_accessor :x, :y, :w, :h, :path, :angle, :a, :r, :g, :b, :source_x,
@@ -58,41 +92,41 @@ end
 
 class Switch < Sprite
   def initialize x, y, w, h
-    self.x = x
-    self.y = y
-    self.w = w
-    self.h = h
-    self.path = 'sprites/specific/switch.png'
+    @x = x
+    @y = y
+    @w = w
+    @h = h
+    @path = 'sprites/specific/switch.png'
   end
 end
 
 class Bulb < Sprite
   def initialize x, y, w, h
-    self.x = x
-    self.y = y
-    self.w = w
-    self.h = h
-    self.path = 'sprites/specific/bulb_unlit.png'
+    @x = x
+    @y = y
+    @w = w
+    @h = h
+    @path = 'sprites/specific/bulb_unlit.png'
   end
 end
 
 class Battery < Sprite
   def initialize x, y, w, h
-    self.x = x
-    self.y = y
-    self.w = w
-    self.h = h
-    self.path = 'sprites/specific/battery.png'
+    @x = x
+    @y = y
+    @w = w
+    @h = h
+    @path = 'sprites/specific/battery.png'
   end
 end 
 
 class Connector < Sprite
   def initialize x, y, w, h
-    self.x = x
-    self.y = y
-    self.w = w
-    self.h = h
-    self.path = 'sprites/specific/connector.png'
+    @x = x
+    @y = y
+    @w = w
+    @h = h
+    @path = 'sprites/specific/connector.png'
   end
 end 
 
@@ -106,9 +140,6 @@ end
 
 def gameplay_tick args
   
-
-  
-
 end
 
 #===========================================================================
@@ -116,5 +147,5 @@ end
 #===========================================================================
 
 def tick args
-  
+  args.outputs.solids << Wire.new(220, 280, 640, 280, 6)
 end
